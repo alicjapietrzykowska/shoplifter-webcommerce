068508fa355111151b7f9df85fad66d136319d5e
Alicja <alicja.x.lukaszewska@gmail.com>
2022-06-14 01:12:51 +0200

    Some optimalizations and pages added

---
+++ src/app/routes/pages/contact/contact.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  template: `contact works!`,
+  styles: [``],
+})
+export class ContactComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}

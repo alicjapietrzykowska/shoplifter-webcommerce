b32509f140f1429a64ece591decaa270a85afc93
Alicja <alicja.x.lukaszewska@gmail.com>
2022-06-15 21:08:05 +0200

    Contact and some optimalizations ready

--- src/app/routes/pages/contact/contact.component.ts
+++ src/app/routes/pages/contact/contact.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
-  template: `contact works!`,
-  styles: [``],
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent implements OnInit {
   constructor() {}

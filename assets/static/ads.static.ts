51971beaaa82bb47b5ddf61258d2772d98d56417
Alicja <alicja.x.lukaszewska@gmail.com>
2022-06-15 23:32:27 +0200

    Auto-generated commit [ci skip]

---
+++ assets/static/ads.static.ts
@@ -0,0 +1,24 @@
+export const adsData = [
+  {
+    title: 'The new season starts here',
+    imageUrl:
+      'https://www.pngall.com/wp-content/uploads/2/Black-Sneakers-PNG.png',
+    category: "women's clothing",
+  },
+  {
+    title: 'Unique jewelry made from genuine materials',
+    imageUrl: 'https://www.pngall.com/wp-content/uploads/2017/01/Pearl-PNG.png',
+    category: 'jewelery',
+  },
+  {
+    title: 'High Tech Electronics',
+    imageUrl:
+      'https://www.pngall.com/wp-content/uploads/12/DSLR-Camera-PNG-Free-Image.png',
+    category: 'electronics',
+  },
+  {
+    title: 'Celebrate expert tailoring and modern luxury',
+    imageUrl: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+    category: "men's clothing",
+  },
+];
---
+++ assets/static/discounts.static.ts
@@ -0,0 +1,11 @@
+import { Discount } from '@interfaces/discountDto';
+export const discountCodes: Discount[] = [
+  {
+    code: '10OFF',
+    value: 10,
+  },
+  {
+    code: '50OFF',
+    value: 50,
+  },
+];
